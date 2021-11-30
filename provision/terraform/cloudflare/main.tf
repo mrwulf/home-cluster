fadb71600da5523ada57696b2c640d2eefe2fe01
bwulf <bwulf@ntent.com>
2021-11-29 20:38:01 -0800

    new cluster w/ age

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -78,23 +78,24 @@ resource "cloudflare_zone_settings_override" "cloudflare_settings" {
   }
 }
 
-data "http" "ipv4" {
-  url = "http://ipv4.icanhazip.com"
-}
+# data "http" "ipv4" {
+#   url = "http://ipv4.icanhazip.com"
+# }
 
-resource "cloudflare_record" "ipv4" {
-  name    = "ipv4"
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = chomp(data.http.ipv4.body)
-  proxied = true
-  type    = "A"
-  ttl     = 1
-}
+# resource "cloudflare_record" "ipv4" {
+#   name    = "ipv4"
+#   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+#   value   = chomp(data.http.ipv4.body)
+#   proxied = true
+#   type    = "A"
+#   ttl     = 1
+# }
 
 resource "cloudflare_record" "root" {
   name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  # value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  value = "dynamic.webwulf.net"
   proxied = true
   type    = "CNAME"
   ttl     = 1
@@ -103,7 +104,8 @@ resource "cloudflare_record" "root" {
 resource "cloudflare_record" "hajimari" {
   name    = "hajimari"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  # value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  value = "dynamic.webwulf.net"
   proxied = true
   type    = "CNAME"
   ttl     = 1
