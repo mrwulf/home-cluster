d49dc25063eb3a37722d2342e25c024bab8058b9
bwulf <mrwulf@users.noreply.github.com>
2023-02-23 17:07:38 -0800

    pgadmin oauth

---
+++ cluster/apps/databases/postgres/pgAdmin/config_local.py
@@ -0,0 +1,15 @@
+AUTHENTICATION_SOURCES = ['oauth2', 'internal']
+OAUTH2_AUTO_CREATE_USER = True
+OAUTH2_CONFIG = [{
+    'OAUTH2_NAME' : 'authentik',
+    'OAUTH2_DISPLAY_NAME' : 'Authentik',
+    'OAUTH2_CLIENT_ID' : '${PGADMIN_OAUTH_CLIENT_ID}',
+    'OAUTH2_CLIENT_SECRET' : '${PGADMIN_OAUTH_SECRET}',
+    'OAUTH2_TOKEN_URL' : 'https://auth.${SECRET_DOMAIN}/application/o/token/',
+    'OAUTH2_AUTHORIZATION_URL' : 'https://auth.${SECRET_DOMAIN}/application/o/authorize/',
+    'OAUTH2_API_BASE_URL' : 'https://auth.${SECRET_DOMAIN}/',
+    'OAUTH2_USERINFO_ENDPOINT' : 'https://auth.${SECRET_DOMAIN}/application/o/userinfo/',
+    'OAUTH2_SCOPE' : 'openid email profile',
+    'OAUTH2_ICON' : 'fa-openid',
+    'OAUTH2_BUTTON_COLOR' : '#2db1fd'
+}]
