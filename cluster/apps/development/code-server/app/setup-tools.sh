dc45747ff666b3767d5dc4dfbeeaad70991391f1
Brandon Wulf <mrwulf@users.noreply.github.com>
2025-12-18 15:46:50 -0500

    feat(code-server): add init container to install development tools

---
+++ cluster/apps/development/code-server/app/setup-tools.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y curl ca-certificates gettext-base bash-completion command-not-found python3-pip
+mkdir -p /home/coder/.local/bin
+cd /home/coder/.local/bin
+
+echo "Installing kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+echo "Installing talosctl..."
+curl -Lo talosctl https://github.com/siderolabs/talos/releases/latest/download/talosctl-linux-amd64
+
+echo "Installing jq..."
+curl -Lo jq https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64
+
+echo "Installing yq..."
+curl -Lo yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+
+chmod +x kubectl talosctl jq yq
+chown -R 1000:1000 /home/coder/.local
