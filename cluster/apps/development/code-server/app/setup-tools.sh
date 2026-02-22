61a3b1a8851e8dc7810a465c5eab2ee76e9981dd
Brandon Wulf <mrwulf@users.noreply.github.com>
2026-02-21 16:08:58 -0800

    install mise

--- cluster/apps/development/code-server/app/setup-tools.sh
+++ cluster/apps/development/code-server/app/setup-tools.sh
@@ -6,17 +6,23 @@ apt update &&
 mkdir -p /home/coder/bin
 cd /home/coder/bin
 
-echo "Installing kubectl..."
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-echo "Installing talosctl..."
-curl -Lo talosctl https://github.com/siderolabs/talos/releases/latest/download/talosctl-linux-amd64
+echo "Installing mise-en-place..."
+curl https://mise.run | sh
 
-echo "Installing jq..."
-curl -Lo jq https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64
+/home/coder/.local/bin/mise prepare
 
-echo "Installing yq..."
-curl -Lo yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+# echo "Installing kubectl..."
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-chmod +x kubectl talosctl jq yq
-chown -R 1000:1000 /home/coder/bin
+# echo "Installing talosctl..."
+# curl -Lo talosctl https://github.com/siderolabs/talos/releases/latest/download/talosctl-linux-amd64
+
+# echo "Installing jq..."
+# curl -Lo jq https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64
+
+# echo "Installing yq..."
+# curl -Lo yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+
+# chmod +x kubectl talosctl jq yq
+# chown -R 1000:1000 /home/coder/bin
