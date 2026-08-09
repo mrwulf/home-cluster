a1f30c55f08456a94173b0749d40d81bd08eafe1
bwulf <mrwulf@users.noreply.github.com>
2026-08-08 22:28:24 -0700

    fix(flux): resolve flux-instance upgrade downgrade stall and flux 2.9 envsubst errors

--- cluster/apps/networking/cloudflare-ddns/app/cloudflare-ddns.sh
+++ cluster/apps/networking/cloudflare-ddns/app/cloudflare-ddns.sh
@@ -6,14 +6,14 @@ set -o errexit
 
 current_ipv4="$(curl -s https://ipv4.icanhazip.com/)"
 zone_id=$(curl -s -X GET \
-    "https://api.cloudflare.com/client/v4/zones?name=${CLOUDFLARE_RECORD_NAME#*.}&status=active" \
-    -H "Authorization: Bearer ${CLOUDFLARE_APIKEY}" \
+    "https://api.cloudflare.com/client/v4/zones?name=$CLOUDFLARE_RECORD_NAME&status=active" \
+    -H "Authorization: Bearer $CLOUDFLARE_APIKEY" \
     -H "Content-Type: application/json" \
         | jq --raw-output ".result[0] | .id"
 )
 record_ipv4=$(curl -s -X GET \
-    "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${CLOUDFLARE_RECORD_NAME}&type=A" \
-    -H "Authorization: Bearer ${CLOUDFLARE_APIKEY}" \
+    "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=$CLOUDFLARE_RECORD_NAME&type=A" \
+    -H "Authorization: Bearer $CLOUDFLARE_APIKEY" \
     -H "Content-Type: application/json" \
 )
 old_ip4=$(echo "$record_ipv4" | jq --raw-output '.result[0] | .content')
@@ -24,9 +24,9 @@ fi
 record_ipv4_identifier="$(echo "$record_ipv4" | jq --raw-output '.result[0] | .id')"
 update_ipv4=$(curl -s -X PUT \
     "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_ipv4_identifier}" \
-    -H "Authorization: Bearer ${CLOUDFLARE_APIKEY}" \
+    -H "Authorization: Bearer $CLOUDFLARE_APIKEY" \
     -H "Content-Type: application/json" \
-    --data "{\"id\":\"${zone_id}\",\"type\":\"A\",\"proxied\":false,\"name\":\"${CLOUDFLARE_RECORD_NAME}\",\"content\":\"${current_ipv4}\"}" \
+    --data "{\"id\":\"${zone_id}\",\"type\":\"A\",\"proxied\":false,\"name\":\"$CLOUDFLARE_RECORD_NAME\",\"content\":\"${current_ipv4}\"}" \
 )
 if [[ "$(echo "$update_ipv4" | jq --raw-output '.success')" == "true" ]]; then
     printf "%s - Success - IP Address '%s' has been updated" "$(date -u)" "${current_ipv4}"
