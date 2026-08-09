26c047c54adb29910e2b7e4f7e8e8e9b56180efd
bwulf <mrwulf@users.noreply.github.com>
2026-08-08 22:33:40 -0700

    fix(flux): resolve flux-instance upgrade downgrade stall and flux 2.9 envsubst errors

--- cluster/apps/networking/cloudflare-ddns/app/cloudflare-ddns.sh
+++ cluster/apps/networking/cloudflare-ddns/app/cloudflare-ddns.sh
@@ -12,26 +12,26 @@ zone_id=$(curl -s -X GET \
         | jq --raw-output ".result[0] | .id"
 )
 record_ipv4=$(curl -s -X GET \
-    "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=$CLOUDFLARE_RECORD_NAME&type=A" \
+    "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?name=$CLOUDFLARE_RECORD_NAME&type=A" \
     -H "Authorization: Bearer $CLOUDFLARE_APIKEY" \
     -H "Content-Type: application/json" \
 )
 old_ip4=$(echo "$record_ipv4" | jq --raw-output '.result[0] | .content')
-if [[ "${current_ipv4}" == "${old_ip4}" ]]; then
-    printf "%s - IP Address '%s' has not changed" "$(date -u)" "${current_ipv4}"
+if [[ "$current_ipv4" == "$old_ip4" ]]; then
+    printf "%s - IP Address '%s' has not changed" "$(date -u)" "$current_ipv4"
     exit 0
 fi
 record_ipv4_identifier="$(echo "$record_ipv4" | jq --raw-output '.result[0] | .id')"
 update_ipv4=$(curl -s -X PUT \
-    "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_ipv4_identifier}" \
+    "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$record_ipv4_identifier" \
     -H "Authorization: Bearer $CLOUDFLARE_APIKEY" \
     -H "Content-Type: application/json" \
-    --data "{\"id\":\"${zone_id}\",\"type\":\"A\",\"proxied\":false,\"name\":\"$CLOUDFLARE_RECORD_NAME\",\"content\":\"${current_ipv4}\"}" \
+    --data "{\"id\":\"$zone_id\",\"type\":\"A\",\"proxied\":false,\"name\":\"$CLOUDFLARE_RECORD_NAME\",\"content\":\"$current_ipv4\"}" \
 )
 if [[ "$(echo "$update_ipv4" | jq --raw-output '.success')" == "true" ]]; then
-    printf "%s - Success - IP Address '%s' has been updated" "$(date -u)" "${current_ipv4}"
+    printf "%s - Success - IP Address '%s' has been updated" "$(date -u)" "$current_ipv4"
     exit 0
 else
-    printf "%s - Yikes - Updating IP Address '%s' has failed" "$(date -u)" "${current_ipv4}"
+    printf "%s - Yikes - Updating IP Address '%s' has failed" "$(date -u)" "$current_ipv4"
     exit 1
 fi
