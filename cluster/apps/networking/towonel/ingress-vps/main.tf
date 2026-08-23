7308bf5ed0452a09533e66548e42159160c44921
bwulf <mrwulf@users.noreply.github.com>
2026-08-22 23:44:49 -0700

    fix(vps): remove unneeded primary v6 record to keep changes strictly scoped to backup VPS

--- cluster/apps/networking/towonel/ingress-vps/main.tf
+++ cluster/apps/networking/towonel/ingress-vps/main.tf
@@ -333,17 +333,6 @@ resource "cloudflare_dns_record" "vps_primary" {
   ttl     = 1
 }
 
-# Primary VPS direct AAAA record
-resource "cloudflare_dns_record" "vps_primary_v6" {
-  count   = local.ovh_primary_ipv6 != "" ? 1 : 0
-  zone_id = data.cloudflare_zones.domain_zones.result[0].id
-  name    = "vps-primary.${var.secret_domain}"
-  content = local.ovh_primary_ipv6
-  type    = "AAAA"
-  proxied = false
-  ttl     = 1
-}
-
 # Backup VPS direct A record
 resource "cloudflare_dns_record" "vps_backup" {
   zone_id = data.cloudflare_zones.domain_zones.result[0].id
